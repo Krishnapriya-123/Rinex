5214eac0ec94e430781fd62917122f77c743a027
Krishnapriya-123 <75377621+Krishnapriya-123@users.noreply.github.com>
2022-09-23 10:31:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import joblib
+
+#load the joblib model
+model_nb=joblib.load('GooglPlayStore')
+
+st.title('GOOGLE PLAYSTORE')
+ip=st.text_input('Enter:')
+
+op=model_nb.predict([ip])
+if st.button('PREDICT'):
+   st.title(op[0])
