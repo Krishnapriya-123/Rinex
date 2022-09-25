dde8dad4d2417a47e00b9c0bd8531110cc47d3dd
Krishnapriya-123 <75377621+Krishnapriya-123@users.noreply.github.com>
2022-09-25 09:56:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 #load the joblib model
 model_nb=joblib.load('googleplaystore')
 
-st.title('googleplaystore')
+st.title('GOOGLE PLAYSTORE CLASSIFIER')
 ip=st.text_input('Enter:')
 
 op=model_nb.predict([ip])
